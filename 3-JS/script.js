93d6931952fba28b54a059dbab5704b55a99dbee
Mustafa Tuncel <mustafatuncel.93@outlook.com>
2022-12-18 11:32:56 +0300

    JS-entrance

---
+++ 3-JS/script.js
@@ -0,0 +1,11 @@
+// yorum satırı
+/* yorum satıri */
+
+alert("merhaba");
+console.log(merhaba);
+console.log(1234);
+console.log(true);
+console.log([1,2,3]);
+console.warn("uyarı");
+console.error("hata oluştu");
+console.clear();
