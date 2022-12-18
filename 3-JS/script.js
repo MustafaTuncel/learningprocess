76cc066c0c6744058d781769c46cec7a2d5a1a40
Mustafa Tuncel <mustafatuncel.93@outlook.com>
2022-12-18 13:18:08 +0300

    edited

--- 3-JS/script.js
+++ 3-JS/script.js
@@ -1,11 +1,10 @@
 // yorum satırı
 /* yorum satıri */
-
-alert("merhaba");
+let merhaba = "merhaba"
+alert("Hoşgeldiniz.");
 console.log(merhaba);
 console.log(1234);
 console.log(true);
 console.log([1,2,3]);
 console.warn("uyarı");
 console.error("hata oluştu");
-console.clear();
