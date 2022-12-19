0e117343a983e35077dcdfb9e8de8bbbcae752be
Mustafa Tuncel <mustafatuncel.93@outlook.com>
2022-12-19 11:46:39 +0300

    Değişkenler added.

--- 3-JS/script.js
+++ 3-JS/script.js
@@ -1,10 +1,20 @@
-// yorum satırı
-/* yorum satıri */
+// Javascript'e Giriş
 let merhaba = "merhaba"
 alert("Hoşgeldiniz.");
 console.log(merhaba);
 console.log(1234);
 console.log(true);
-console.log([1,2,3]);
+console.log([1, 2, 3]);
 console.warn("uyarı");
 console.error("hata oluştu");
+
+// Değişkenler
+var age;
+console.log(age); // Undefined (atama yapılmamış) Değişken
+
+age = 20;
+console.log(age); // 'Sayısal değer atanmış'değişken
+
+var fullname = "Mustafa Tuncel";
+console.log(fullname);
+
