c0d6b38ca9f99fe263102616fee4e4c2f32f8aa3
Mustafa Tuncel <mustafatuncel.93@outlook.com>
2022-12-21 11:13:02 +0300

    Variable Types added.

--- 3-JS/script.js
+++ 3-JS/script.js
@@ -1,4 +1,4 @@
-// Javascript'e Giriş
+    // Javascript'e Giriş
 let merhaba = "merhaba"
 alert("Hoşgeldiniz.");
 console.log(merhaba);
@@ -8,13 +8,39 @@ console.log([1, 2, 3]);
 console.warn("uyarı");
 console.error("hata oluştu");
 
-// Değişkenler
+    // Değişkenler
 var age;
 console.log(age); // Undefined (atama yapılmamış) Değişken
 
 age = 20;
-console.log(age); // 'Sayısal değer atanmış'değişken
+console.log(age); // Sayısal değer atanmış değişken
 
 var fullname = "Mustafa Tuncel";
-console.log(fullname);
+console.log(fullname); // Sözel değer atanmış değişken
 
+    // Değişken Tipleri
+    // a-Primitive Types
+// Undefined
+let car;
+// String
+let firstname = "Çınar";
+// Number
+let age = 20;
+// Boolean
+let IsActive = true
+console.log(IsActive);
+// Null
+let job = null;
+
+    // b-Reference Types - Objects
+// Array
+let names = ["Ali", "Ahmet", "Can"];
+// Object Literals
+let address = {
+    city: "Tekirdağ",
+    country: "Türkiye"
+};
+// Function
+let calculateAge = function () {
+    return 0;
+};
